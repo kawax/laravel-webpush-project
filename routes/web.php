f1322b55bfa2beb89cf687370c5486150eb26be5
kawax <kawaxbiz@gmail.com>
2019-02-07 19:54:37 +0900

    webpush

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+// Notifications
+Route::post('notifications', 'NotificationController@store');
+Route::get('notifications', 'NotificationController@index');
+Route::patch('notifications/{id}/read', 'NotificationController@markAsRead');
+Route::post('notifications/mark-all-read', 'NotificationController@markAllRead');
+Route::post('notifications/{id}/dismiss', 'NotificationController@dismiss');
+
+// Push Subscriptions
+Route::post('subscriptions', 'PushSubscriptionController@update');
+Route::post('subscriptions/delete', 'PushSubscriptionController@destroy');
+
+// Manifest file (optional if VAPID is used)
+//Route::get('manifest.json', function () {
+//    return [
+//        'name'          => config('app.name'),
+//        'gcm_sender_id' => config('webpush.gcm.sender_id'),
+//    ];
+//});
